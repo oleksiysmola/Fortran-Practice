44360db5abcc88d29866671ec35fc8d9a39cf431
alex smola <asmola@frost.theory.phys.ucl.ac.uk>
2022-09-06 13:06:24 +0100

    completed exercise 2

--- FortranProgramme.f90
+++ FortranProgramme.f90
@@ -12,6 +12,11 @@ PROGRAM FortranProgramme ! Indicates start of programme
     j = i**2
     k = i - j
 
-    WRITE(*,*) "The value of x is", x, "and value of y is", y
+    WRITE(*,*) "The value of x is", x
+    WRITE(*,*) "The value of y is", y
+    WRITE(*,*) "The value of z is", z
+    WRITE(*,*) "The value of i is", i
+    WRITE(*,*) "The value of j is", j
+    WRITE(*,*) "The value of k is", k
     
 END PROGRAM FortranProgramme ! Indicates end of programme 
