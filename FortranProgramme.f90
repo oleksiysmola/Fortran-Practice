d89aec6b6fc460e0f0d38391dde04e0af80c7c42
alex smola <asmola@frost.theory.phys.ucl.ac.uk>
2022-09-06 11:59:51 +0100

    first commit

---
+++ FortranProgramme.f90
@@ -0,0 +1,15 @@
+PROGRAM FortranProgramme ! Indicates start of programme 
+    IMPLICIT NONE
+
+    INTEGER :: i, j, k
+    REAL :: x, y, z
+    
+    x = 3.14
+    y = cos(x)
+    z = x + y
+    
+    i = 3
+    j = i**2
+    k = i - j
+    
+END PROGRAM FortranProgramme ! Indicates end of programme 
---
+++ README.md
@@ -0,0 +1,2 @@
+## Fortran Practice
+This repository is for me to upload any material I've been using and code I've been writing to help practice the Fortran programming language.
