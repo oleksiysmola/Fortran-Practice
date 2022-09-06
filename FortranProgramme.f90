a1937b90c03f40a91a70cda9f99eb18416d31b2c
alex smola <asmola@frost.theory.phys.ucl.ac.uk>
2022-09-06 13:14:46 +0100

    now writes variable in file

--- FortranProgramme.f90
+++ FortranProgramme.f90
@@ -12,11 +12,16 @@ PROGRAM FortranProgramme ! Indicates start of programme
     j = i**2
     k = i - j
 
+    ! Prints specified values
     WRITE(*,*) "The value of x is", x
     WRITE(*,*) "The value of y is", y
     WRITE(*,*) "The value of z is", z
     WRITE(*,*) "The value of i is", i
     WRITE(*,*) "The value of j is", j
     WRITE(*,*) "The value of k is", k
+
+    ! Writes to file
+    OPEN(10,file="data.dat")
+    WRITE(10,*) "The value of x is ", x
     
 END PROGRAM FortranProgramme ! Indicates end of programme 
