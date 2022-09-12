43e38c494b6938112c399ace53ef6a8c51e48b37
alex smola <asmola@frost.theory.phys.ucl.ac.uk>
2022-09-12 15:01:32 +0100

    added program for simple fortran function

---
+++ FortranFunctions.f90
@@ -0,0 +1,23 @@
+PROGRAM FortranFunctions
+    IMPLICIT NONE
+    INTEGER :: i
+    REAL :: x
+    REAL, PARAMETER :: xmin = 0.,xmax=10., a=-2.
+    
+    OPEN(10,file="myplot.dat")
+    
+    DO i = 1,100
+        x = xmin + xmax*(i-1.0)/(100.0-1.0)
+        WRITE(10,*) x,f(x)
+    ENDDO
+    
+    CLOSE(10)
+    CONTAINS
+    
+    FUNCTION f(x)
+    IMPLICIT NONE
+    REAL :: f,x
+    f = cos(x+a)
+    END FUNCTION f
+    
+END PROGRAM FortranFunctions
